6f950abf2bd1c3cdb69aca494d5c2480a1013670
Ghouse <mohammed.shaikh@niit.com>
2019-10-17 23:05:54 +0530

    Initial Commit

---
+++ src/main/java/com/stackroute/GradeCalculator.java
@@ -0,0 +1,17 @@
+package com.stackroute;
+
+import java.util.Map;
+
+public class GradeCalculator {
+
+	public static Map<Integer, String> calculateGrade(Map<Integer, Integer> scores) {
+		return null;
+	}
+
+   	//Do not print anything other than what's expected/asked in problem
+    public static void main(String[] args) {
+    	//Use Scanner to get input from console
+
+
+	}
+}
